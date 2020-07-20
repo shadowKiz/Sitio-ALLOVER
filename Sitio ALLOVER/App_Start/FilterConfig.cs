979528af3dafb4777bb834b012a5a0a7bbc69109
solid <solid@DESKTOP-N491H8S>
2020-07-20 03:09:16 -0500

    Agregar archivos de proyecto.

---
+++ Sitio ALLOVER/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Sitio_ALLOVER
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Sitio ALLOVER/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sitio_ALLOVER.Startup))]
+namespace Sitio_ALLOVER
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
